ca775a27b103bbbaffcd4d2105711194ba9854da
Satsuki Akiba <anosatsuk124@gmail.com>
2023-05-26 17:06:41 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,11 @@ pub mod watch;
 
 use std::env;
 
-pub fn init_logger() -> anyhow::Result<()> {
-    if env::var("RUST_LOG").is_ok() {
+pub fn init_logger(mode: &RunMode) -> anyhow::Result<()> {
+    if mode != &RunMode::Gui {
+        drop(env_logger::try_init()?);
+        log::info!("Logger initialized with env_logger.");
+    } else if env::var("RUST_LOG").is_ok() {
         drop(env_logger::try_init()?);
         log::info!("Logger initialized with env_logger.");
     } else {
@@ -24,24 +27,60 @@ pub fn init_locale() {
     let locale = if let Ok(locale) = env::var("LOCALE") {
         locale
     } else if let Some(locale) = sys_locale::get_locale() {
-        available_locales()
-            .iter()
-            .find(|l| l.to_string() == locale)
-            .map(|l| l.to_string())
-            .unwrap_or(DEFAULT_LOCALE.to_string())
+        locale
     } else {
         DEFAULT_LOCALE.to_string()
     };
+
+    let locale = available_locales()
+        .iter()
+        .find(|l| l.to_string() == locale)
+        .map(|l| l.to_string())
+        .unwrap_or(DEFAULT_LOCALE.to_string());
 
     rust_i18n::set_locale(&locale);
     log::info!("Locale set to {}.", locale);
 }
 
-pub fn run() -> anyhow::Result<()> {
-    init_logger()?;
+pub fn gui_run() -> anyhow::Result<()> {
+    init_logger(&RunMode::Gui)?;
     init_locale();
 
     app::app()?;
 
     Ok(())
+}
+
+/// NOTE: Not implemented yet.
+pub fn cli_run(mode: RunMode) -> anyhow::Result<()> {
+    init_logger(&mode)?;
+    init_locale();
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq)]
+pub enum RunMode {
+    Gui,
+    Deamon,
+    /// NOTE: Not implemented yet.
+    Interactive,
+}
+
+const RUN_MODE_ENV: &str = "RUN_MODE";
+pub fn run() -> anyhow::Result<()> {
+    match env::var(RUN_MODE_ENV) {
+        Ok(mode) => {
+            let mode = mode.as_str();
+            match mode {
+                "deamon" => cli_run(RunMode::Deamon),
+                "interactive" => cli_run(RunMode::Interactive),
+                "gui" => gui_run(),
+                _ => {
+                    anyhow::bail!("Invalid RUN_MODE: {}", mode)
+                }
+            }
+        }
+        Err(_) => gui_run(),
+    }
 }
